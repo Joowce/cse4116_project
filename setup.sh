f3fb68973d5252743602ce2cfee96fd5e192471b
KOREAN139 <korean139@gmail.com>
2019-06-28 03:53:42 +0900

    <chore> remove fpga module insert line in setup.sh
    script in /fpga_modules have same functionality

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 echo "7 6 1 7" > /proc/sys/kernel/printk
 
 # insmod & mknod
-insmod fpga_push_switch_driver.ko
 insmod device_driver/huins.ko
 mknod /dev/huins_driver c 242 0
-mknod /dev/fpga_push_switch_driver c 265 0
 chomd 777 /dev/huins_driver
