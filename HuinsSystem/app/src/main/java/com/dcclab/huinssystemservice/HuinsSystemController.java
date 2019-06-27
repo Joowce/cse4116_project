a535b7eb584b1c9d79db6ef07b8e5a4859bf8d01
Joowce <minjw1026@naver.com>
2019-06-27 22:13:46 +0900

    add: - HuinsSystemController --- remove: --- change:

--- HuinsSystem/app/src/main/java/com/dcclab/huinssystemservice/HuinsSystemController.java
+++ HuinsSystem/app/src/main/java/com/dcclab/huinssystemservice/HuinsSystemController.java
@@ -1,4 +1,45 @@
 package com.dcclab.huinssystemservice;
 
+
 public class HuinsSystemController {
+    private HandlerSystemInput handler;
+    private Runnable thread;
+
+    HuinsSystemController(HandlerSystemInput handler) {
+        setHandler(handler);
+        init();
+    }
+
+    public void setHandler(HandlerSystemInput handler) {
+        this.handler = handler;
+    }
+
+    public void start() {
+        if (thread != null) return;
+
+        thread = new Runnable() {
+            @Override
+            public void run () {
+                while(true) {
+                    int input = listen();
+                    if (handler.handleInput(input) <= 0) break;
+                }
+            }
+        };
+        thread.run();
+    }
+
+    public void stop() {
+        end();
+        thread = null;
+    }
+
+
+    private native void init();
+    private native int listen();
+    private native void end();
+
+    public interface HandlerSystemInput {
+        int handleInput(int input);
+    }
 }
