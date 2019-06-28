b144de11d4cfb0ec74e26876ae91c85fa3e82d12
Joowce <minjw1026@naver.com>
2019-06-28 09:39:09 +0900

    add: - demo-app v1: LED, FND, LCD --- remove: --- change:

--- secureATM/app/src/main/java/com/example/secureatm/mainMenu/MainMenuActivity.java
+++ secureATM/app/src/main/java/com/example/secureatm/mainMenu/MainMenuActivity.java
@@ -2,49 +2,57 @@ package com.example.secureatm.mainMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TableLayout;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.example.secureatm.R;
-import com.google.android.material.button.MaterialButton;
 
-import butterknife.BindArray;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import android.os.HuinsSystemManager;
+
 
 public class MainMenuActivity extends AppCompatActivity {
 
     private String LOG_TAG = MainMenuActivity.class.getName();
-    @BindView(R.id.menuContainer)
-    public TableLayout menuContainer;
-    @BindArray(R.array.tasks)
-    public String[] tasks;
+    private boolean[] ledPressed;
+    private HuinsSystemManager huinsService;
+    @BindView(R.id.FND_Input) private EditText FNDInput;
+    @BindView(R.id.LCD_input) private EditText LCDInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setupMenuContent(tasks);
+        huinsService = (HuinsSystemManager) getApplicationContext()
+                .getSystemService(Context.HUINS_SYSTEM_SERVICE);
+        huinsService.init();
     }
 
-    private void setupMenuContent(String[] tasks) {
-        for (String task : tasks) {
-            MaterialButton btn = new MaterialButton(this);
-            btn.setGravity(Gravity.CENTER);
-            btn.setText(task);
-            menuContainer.addView(btn);
-            btn.setOnClickListener((View view) -> {
-                String btnText = ((Button) view).getText().toString();
-                switch (btnText) {
-                    case "":
-                }
-            });
-        }
+    @OnClick({R.id.radioButton1, R.id.radioButton2, R.id.radioButton3, R.id.radioButton4,
+            R.id.radioButton5, R.id.radioButton6, R.id.radioButton7, R.id.radioButton8})
+    public void handleRadioButtons(View button) {
+        RadioButton radioButton = (RadioButton) button;
+        int id = Integer.parseInt(radioButton.getText().toString());
+        ledPressed[id] = radioButton.isChecked();
+        huinsService.writeLED(ledPressed);
     }
 
+    @OnClick(R.id.FND_button)
+    public void handleFND(View textIput) {
+        int num = Integer.parseInt(((EditText) textIput).getText().toString());
+        huinsService.writeFND(num);
+    }
+
+    @OnClick(R.id.LCD_input)
+    public void handleLCD(View textInput) {
+        String str = ((EditText) textInput).getText().toString();
+        huinsService.writeLCD(str);
+    }
 }
